8429978043e6e61382acb4a9187a46057cd8d8da
LAPTOP-GA9EKJT2\BORIS <borispipo@gmail.com>
2024-01-13 07:34:06 +0100

    Ajout de l'alias FILE

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const path = require("path");
 const deleteFileOrDirectory = require("./src/deleteFileOrDirectory");
 const JSONFileManager = require("./src/JSONFileManager");
 const session = require("./src/session");
+const FILE = require("./src/file");
 module.exports = {
     ...require("./src/object"),
     Config : require("./src/config"),
@@ -13,7 +14,6 @@ module.exports = {
     writeFile : require("./src/writeFile"),
     isWritable : require("./src/isWritable"),
     copy : require("./src/copy"),
-    electronDir : path.resolve(__dirname, ".."),
     isRegExp : require("./src/isRegex"),
     paths : require("./src/paths"),
     exec : require("./src/exec"),
@@ -33,7 +33,8 @@ module.exports = {
     isValidUrl : require("./src/isValidUrl"),
     createDirSync : require("./src/createDirSync"),
     isObj : x=> typeof x =="object" && x && !Array.isArray(x),
-    file : require("./src/file"),
+    file : FILE,
+    FILE,
     isNonNullString : require("./src/isNonNullString"),
     JSONManager : JSONFileManager,
     JSONFileManager,
