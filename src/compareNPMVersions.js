90f0fc1a7554bab27a960c801251f38ad5201ded
LAPTOP-GA9EKJT2\BORIS <borispipo@gmail.com>
2024-06-14 07:17:53 +0100

    Update de la fonction compareNPMVersion et fix bug

--- src/compareNPMVersions.js
+++ src/compareNPMVersions.js
@@ -3,22 +3,54 @@
     compare two npm packages versions
     @param {string} current, current version
     @param {string} other, other version
+    @param {options} options object : 
     @return {int}, if 0 then versions are equals, if -1 then current are less than other if 1 then current are greater than other
 */
-module.exports = function compareNPMVersions(current, other) {
-    var cp = String(current).split('.');
-    var op = String(other).split('.');
-    for (var depth = 0; depth < Math.min(cp.length, op.length); depth++) {
-        var cn = Number(cp[depth]);
-        var on = Number(op[depth]);
-        if (cn > on)
-            return 1;
-        if (on > cn)
-            return -1;
-        if (!isNaN(cn) && isNaN(on))
-            return 1;
-        if (isNaN(cn) && !isNaN(on))
-            return -1;
+module.exports = function compareNPMVersions(v1, v2, options) {
+    v1 = typeof v1 =="string" && v1 || '';
+    v2 = typeof v2 =="string" && v2 || "";
+    var lexicographical = options && options.lexicographical,
+        zeroExtend = options && options.zeroExtend,
+        v1parts = v1.split('.'),
+        v2parts = v2.split('.');
+
+    function isValidPart(x) {
+        return (lexicographical ? /^\d+[A-Za-z]*$/ : /^\d+$/).test(x);
     }
+
+    if (!v1parts.every(isValidPart) || !v2parts.every(isValidPart)) {
+        return NaN;
+    }
+
+    if (zeroExtend) {
+        while (v1parts.length < v2parts.length) v1parts.push("0");
+        while (v2parts.length < v1parts.length) v2parts.push("0");
+    }
+
+    if (!lexicographical) {
+        v1parts = v1parts.map(Number);
+        v2parts = v2parts.map(Number);
+    }
+
+    for (var i = 0; i < v1parts.length; ++i) {
+        if (v2parts.length == i) {
+            return 1;
+        }
+
+        if (v1parts[i] == v2parts[i]) {
+            continue;
+        }
+        else if (v1parts[i] > v2parts[i]) {
+            return 1;
+        }
+        else {
+            return -1;
+        }
+    }
+
+    if (v1parts.length != v2parts.length) {
+        return -1;
+    }
+
     return 0;
 }
